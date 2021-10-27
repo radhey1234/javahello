1a0c14fa8498442d2666898a4b9c07fa70388e8d
radhey1234 <radhey03d@gmail.com>
2021-10-27 22:06:04 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class hello{  
     public static void main(String args[]){  
-     System.out.println("Hellorerer Java"); 
+     System.out.println("Hellorerer Javaere"); 
      System.out.println("rahey");   
     System.out.println("hi radhey java and 1234 program"); 
     }  
