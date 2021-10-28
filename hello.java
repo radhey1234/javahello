77e5b153ebd40a51264bc211eac2cef233537117
root <root@ip-172-31-22-64.ap-southeast-1.compute.internal>
2021-10-28 18:12:00 +0000

    raaaaa

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class hello{
     public static void main(String args[]){  
      System.out.println("Hellorerer Javaerert"); 
      System.out.println("rahey");
-    System.out.println("radhey2");   
+    System.out.println("radhey23434");   
     System.out.println("hi radhey java and 1234 program"); 
     }  
 }  
