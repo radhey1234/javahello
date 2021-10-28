7c1057cd8dd59f6955ffaa93d42bafd8e8964d0f
root <root@ip-172-31-22-64.ap-southeast-1.compute.internal>
2021-10-28 18:04:07 +0000

    one radhey addedd

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 class hello{  
     public static void main(String args[]){  
      System.out.println("Hellorerer Javaerert"); 
-     System.out.println("rahey");   
+     System.out.println("rahey");
+    System.out.println("radhey2");   
     System.out.println("hi radhey java and 1234 program"); 
     }  
 }  
